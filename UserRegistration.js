13b47e049eca89fe19f53b2b910b4ba772158d93
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-02 23:59:53 +0530

    Added: Regex to check if the entered last name is valid

--- UserRegistration.js
+++ UserRegistration.js
@@ -3,8 +3,19 @@ const input = require('prompt-sync')();
 
 let regexToCheck = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
 
-const firstName = input("Enter Firs Name: ");
-if(regexToCheck.test(firstName))
-    console.log("Valid Name");
-else
-    console.log("Invalid Name");
+const firstName = input("Enter First Name: ");
+
+function checkRegex(inputString) {
+	return regexToCheck.test(inputString);
+}
+
+if (checkRegex(firstName)) {
+const lastName = input("Enter Last Name: ");
+	if (checkRegex(lastName)) 
+			console.log("Valid name");
+	else
+			console.log("Invalid last name");
+}
+else {
+	console.log("Invalid first name");
+}
