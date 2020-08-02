df342ec407d31120489a2f99962e6e8f210d3f00
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-02 23:30:56 +0530

    Added: Regex to check if the entered name is valid

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,1 +1,10 @@
 console.log("Welcome to User Registration Problem");
+const input = require('prompt-sync')();
+
+let regexToCheck = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+
+const firstName = input("Enter Firs Name: ");
+if(regexToCheck.test(firstName))
+    console.log("Valid Name");
+else
+    console.log("Invalid Name");
